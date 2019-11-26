6033407af6a3bc974eaed36264f96f22d8ad9e6c
Billy Ellis <billy@zygosec.com>
2019-11-26 14:10:02 +0000

    Refactor/add comments

--- LinkedLists/linked_list.c
+++ LinkedLists/linked_list.c
@@ -9,18 +9,34 @@ struct list_element{
 };
 
 int main(){
-
+	// allocate 3 list elements on the heap
 	struct list_element *first_elem = malloc(sizeof(struct list_element));
 	struct list_element *second_elem = malloc(sizeof(struct list_element));
+	struct list_element *third_elem = malloc(sizeof(struct list_element));
 
+	// assign each element a value and a pointer to the next element
 	first_elem->value = 41;
 	first_elem->next = second_elem;
-	
 	second_elem->value = 42;
-	second_elem->next = NULL;	
+	second_elem->next = third_elem;	
+	third_elem->value = 43;
+	third_elem->next = NULL;	
 	
-	printf("List elem 1 has value %d and next %p\n", first_elem->value, first_elem->next);
-	printf("List elem 2 has value %d and next %p\n", first_elem->next->value, first_elem->next->next);
+	// iterate through the list elements, starting with the first, by following the 'next' pointers
+	struct list_element *current = first_elem;
+	while (1){
+		printf("Element value %d\n",current->value);
+		if (current->next == NULL){
+			break;
+		}
+		// assign current a pointer to the next element
+		current = current->next;
+	}
+		
+	// free the list elements after use
+	free(first_elem);
+	free(second_elem);
+	free(third_elem);
 	
 	return 0;
 }
