34ac9b6dada097a3ca026bbe65724a063ddda021
James Duffy <james@duffy.app>
2019-12-15 15:35:40 +0000

    .

--- ApplesAndOranges/V5 UserInput - ScanF/cmake-build-debug/cmake_install.cmake
+++ ApplesAndOranges/V5 UserInput - ScanF/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Volumes/JDT5/Projects/Learning C/GithubSync/ApplesAndOranges/V5 UserInput - ScanF
+# Install script for directory: /Users/jamesduffy/Documents/GitHub/LearningC/ApplesAndOranges/V5 UserInput - ScanF
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Volumes/JDT5/Projects/Learning C/GithubSync/ApplesAndOranges/V5 UserInput - ScanF/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/jamesduffy/Documents/GitHub/LearningC/ApplesAndOranges/V5 UserInput - ScanF/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
