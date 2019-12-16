ee31957deb2b45e1bce245ec037721f2f2ca2972
James Duffy <james@duffy.app>
2019-12-16 15:28:27 +0000

    IgnoreThis

---
+++ IgnoreThis/cmake-build-debug/CMakeFiles/IgnoreThis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IgnoreThis.dir/main.c.o"
+  "IgnoreThis"
+  "IgnoreThis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IgnoreThis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
